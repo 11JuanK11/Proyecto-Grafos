bdd56ee58ba74692f05b49dc1c897363a46fd585
Caro-26S <sara_sanchez82221@elpoli.edu.co>
2023-06-08 20:16:37 -0500

    Final

--- src/grafos/Grafo.java
+++ src/grafos/Grafo.java
@@ -2,6 +2,7 @@ package grafos;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.util.ArrayList;
 
 public class Grafo {
 
@@ -62,13 +63,11 @@ public class Grafo {
             M[C][F] = Integer.parseInt(L[i + 2]);
         }
 
-        //Mostrar sencilla matriz
-        
         setM(M);
     }
-    
-    public void MostrarMatrizAdy(){
-        for(int j = 0; j < M.length; j++) {
+
+    public void MostrarMatrizAdy() {
+        for (int j = 0; j < M.length; j++) {
             for (int i = 0; i < M[0].length; i++) {
                 System.out.print(M[j][i]);
             }
@@ -110,15 +109,173 @@ public class Grafo {
         }
     }
     
+    public void CrearMatrizInc(String[] L, String[] V){
+        int[][] MI = new int[V.length][L.length/3];
+        int NumDato1 = 0, NumDato2  = 0;
+        
+        for (int i = 0; i < L.length; i+=3) {
+            int j=0;
+            while (j < V.length){
+                if (V[j].equals(L[i])){
+                    NumDato1 = j;
+                }
+                j++;
+            }
+            j = 0;
+            while (j < V.length){
+                if (V[j].equals(L[i+1])){
+                    NumDato2 = j;
+                }
+                j++;
+            }
+            
+            if (NumDato1 == NumDato2){
+                MI[i][i] = 3;
+            }else{
+                MI[NumDato1][i/3] = 1;
+                MI[NumDato2][i/3] = 2;
+            }
+        }
+        MostrarMatrizInc(MI);
+    }
+    
+    public void MostrarMatrizInc(int[][] MI){
+        for (int j = 0; j < MI.length; j++) {
+            for (int i = 0; i < MI[0].length; i++) {
+                System.out.print(MI[j][i]);
+            }
+            System.out.println("");
+        }
+    }
+    
+    public void MinDistance(int min){
+        int dm[][] = new int[M.length][M.length];
+        ArrayList<Integer> vis = new ArrayList();
+        int i =0, k =0;
+        while(k < dm.length){
+            for (int j = 0; j < M.length; j++) {
+                if(M[i][j] != 0 && !content(j, vis))
+                    dm[j][k] = M[i][j] + dm[i][k];
+            }
+            vis.add(i);
+            int d = min;
+            for (int j = 0; j < M.length; j++) {
+                if(d > dm[j][k] && dm[j][k] != 0 && j != vis.get(k)){
+                    d = dm[j][k];
+                    i = j;
+                }
+            }
+            if(k == 0){
+                dm[i][k+1] = d;
+            }else{
+                for (int j = 0; j < k; j++) {
+                    if(d > dm[i][j] && dm[i][j] != 0 )
+                        d = dm[i][j];      
+                }
+                if (k < M.length-1) 
+                    dm[i][k+1] = d;
+            }
+            k++;
+            if(k == (M.length)-1)
+                System.out.println("Distancia minima: " + dm[k][k]);
+        }
+    }
+    
+    private boolean content(int j, ArrayList<Integer> vis){
+        boolean ban = false;
+        for (Integer vi : vis) {
+            if( vi.intValue() == j)
+                ban = true;
+        }
+        return ban;
+    }
+
+    public void DFS(String S, int[] Visitado, String[] Ve) {
+        Nodo P;
+        int V = 0, j = 0;
+        String W;
+        while (j < Ve.length) {
+            if (Ve[j].equals(S)) {
+                V = j;
+            }
+            j++;
+        }
+
+        Visitado[V] = 1;
+        System.out.print(S + " ");
+        P = Vect[V];
+
+        while (P != null) {
+            W = P.getDato();
+            j = 0;
+            while (j < Ve.length) {
+                if (Ve[j].equals(W)) {
+                    V = j;
+                }
+                j++;
+            }
+            if (Visitado[V] == 0) {
+                DFS(W, Visitado, Ve);
+            }
+            P = P.getLiga();
+        }
+    }
+
+    public void BFS(String S, int[] Visitado, String[] Ve, String[] Cola) {
+        int Primero = -1, Ultimo = -1, j = 0, V = 0;
+        String W;
+        Nodo P;
+
+        while (j < Ve.length) {
+            if (Ve[j].equals(S)) {
+                V = j;
+            }
+            j++;
+        }
+        Visitado[V] = 1;
+        Ultimo++;
+        Cola[Ultimo] = S;
+
+        while (Primero != Ultimo) {
+            Primero++;
+            j = 0;
+            while (j < Ve.length) {
+                if (Ve[j].equals(Cola[Primero])) {
+                    V = j;
+                }
+                j++;
+            }
+            System.out.print(Cola[Primero] + " ");
+            P = Vect[V];
+            
+            while (P != null) {
+                W = P.getDato();
+                j = 0;
+                while (j < Ve.length) {
+                    if (Ve[j].equals(W)) {
+                        V = j;
+                    }
+                    j++;
+                }
+                if (Visitado[V] == 0) {
+                    Visitado[V] = 1;
+                    Ultimo++;
+                    Cola[Ultimo] = W;
+                }
+                P = P.getLiga();
+            }
+        }
+    }
+
     public void Mostrar_Grafo(String V[]) {
-        int[][] Arbol = M;
+        int[][] MA = M;
         String A = "";
         int p = 0;
-        for (int i = 0; i < Arbol.length; i++) {
-            int j = 0;
-            while (j < Arbol[0].length) {
-                if (Arbol[i][j] != 0) {
-                    A += ( V[i] + "->" + V[j] + "\n");
+        for (int i = 0; i < MA.length; i++) {
+            int j = i+1;
+            while (j < MA[0].length) {
+                if (MA[i][j] != 0) {
+                    A += ( V[i] + "--" + V[j] + "\n");
                 }
                 j++;
                 
@@ -132,7 +289,7 @@ public class Grafo {
         try {
             FileWriter a = new FileWriter("ImagenGrafo.txt");
             BufferedWriter escribir = new BufferedWriter(a);
-            escribir.write("digraph ImagenGrafo\n{\n");
+            escribir.write("graph ImagenGrafo\n{\nlayout=neato;\n");
             escribir.write(A);
             escribir.write("}");
             escribir.close();
@@ -160,7 +317,7 @@ public class Grafo {
 
             rt.exec(cmd);
 
-        } catch (Exception ex) {}
+        } catch (Exception ex) {}   
     }
 
 }
--- src/grafos/ProyectoGrafos.java
+++ src/grafos/ProyectoGrafos.java
@@ -6,7 +6,8 @@ public class ProyectoGrafos {
 
     public static void main(String[] args) {
         Scanner Leer = new Scanner(System.in);
-        String Ver = "", Lad = "";
+        String Ver, Lad;
+        int Opc;
 
         System.out.println("Ingrese los vertices separadas por comas: ");
         Ver = Leer.next();
@@ -16,11 +17,100 @@ public class ProyectoGrafos {
         String[] V = Ver.split(",");
         String[] L = Lad.split(",");
 
+        while (L.length % 3 != 0) {
+            System.out.println("Hubo un problema con el ingreso de los lados.: ");
+            System.out.println("Ingrese de nuevo los lados y su distancia: ");
+            Lad = Leer.next();
+            L = Lad.split(",");
+        }
+
         Grafo G = new Grafo(V.length);
         G.CrearMatrizAdy(V, L);
-        G.CrearListaAdy(G.getM(), V);
-        G.MostrarListaAdy();
-        G.Mostrar_Grafo(V);
+
+        do {
+            System.out.println("");
+            System.out.println("MENU PRINCIPAL");
+            System.out.println("1. Mostrar Matriz Adyacente.\n"
+                    + "2. Mostrar Lista Adyacente.\n"
+                    + "3. Mostrar Grafo. \n"
+                    + "4. Matriz de Incidencia. \n"
+                    + "5. Distancia Minima. \n"
+                    + "6. Recorridos. \n"
+                    + "0. Salir.");
+            Opc = Leer.nextInt();
+
+            switch (Opc) {
+                case 1:
+                    System.out.println("Matriz Adyacencia");
+                    G.MostrarMatrizAdy();
+                    break;
+
+                case 2:
+                    System.out.println("Lista de Adyacencia");
+                    G.CrearListaAdy(G.getM(), V);
+                    G.MostrarListaAdy();
+                    break;
+
+                case 3:
+                    G.Mostrar_Grafo(V);
+                    break;
+
+                case 4:
+                    System.out.println("Matriz Incidencia");
+                    G.CrearMatrizInc(L, V);
+                    break;
+
+                case 5:
+                    int Min = 0;
+                    for (int i = 2; i < L.length; i += 3) {
+                        Min += Integer.parseInt(L[i]);
+                    }
+                    G.MinDistance(Min);
+                    break;
+
+                case 6:
+                    do {
+                        System.out.println("");
+                        System.out.println("MENU RECORRIDOS");
+                        System.out.println("1. DFS.\n"
+                                + "2. BFS.\n"
+                                + "3. Volver.");
+                        Opc = Leer.nextInt();
+
+                        switch (Opc) {
+                            case 1:
+                                System.out.println("Ingrese el dato desde el cual desea hacer el recorrido: ");
+                                String SD = Leer.next();
+
+                                int[] Visitado = new int[V.length];
+                                G.DFS(SD, Visitado, V);
+                                break;
+
+                            case 2:
+                                System.out.println("Ingrese el dato desde el cual desea hacer el recorrido: ");
+                                String SB = Leer.next();
+
+                                String[] Cola = new String[V.length];
+                                int[] Visitado2 = new int[V.length];
+                                G.BFS(SB, Visitado2, V, Cola);
+                                break;
+
+                            case 3:
+                                break;
+
+                            default:
+                                System.out.println("Opcion incorrecta.");
+                        }
+                    } while (Opc != 3);
+                    break;
+
+                case 0:
+                    break;
+
+                default:
+                    System.out.println("Opcion incorrecta.");
+            }
+        } while (Opc != 0);
     }
 
 }
